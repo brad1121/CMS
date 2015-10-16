8ccd6d380d10b8544046ff0e211cdc43b675f8ac
Brad <brad1121@gmail.com>
2015-10-16 11:35:34 +1100

    Minor changes

--- CMS/README.md
+++ CMS/README.md
@@ -1,3 +1,6 @@
 ﻿# CMS
 
+On the off chance you are looking at this on github.
+You need to rename db.js.temp to db.js and enter your mongoDB connection info
+
 
--- CMS/routes/index.js
+++ CMS/routes/index.js
@@ -1,12 +1,12 @@
 ﻿var express = require('express');
 var Account = require('../models/account');
 var router = express.Router();
-
+var CMS_Title = "Node CMS";
 module.exports = function (passport) {
     
     /* GET home page. */
     router.get('/', function (req, res) {
-        res.render('index', { title: 'Stanford Marketing' });
+        res.render('index', { title: CMS_Title });
     });
        
     router.post('/register', function (req, res) {
@@ -27,18 +27,23 @@ module.exports = function (passport) {
     router.post('/login', 
         passport.authenticate('local', {
             failureRedirect: '/',
-            successRedirect: '/user'
+            successRedirect: '/dashboard'
         })
     );
     router.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
-
+    router.get('/dashboard', function (req, res) {
+        if (req.session.passport.user === undefined) { res.redirect('/') }
+        else {
+            res.render('dashboard', { title: CMS_Title, user: req.user })
+        }
+    });
     router.get('/user', function (req, res) {
         if (req.session.passport.user === undefined) { res.redirect('/') }
         else {
-            res.render('user', { title: 'Stanford Marketing', user: req.user })
+            res.render('user', { title: CMS_Title, user: req.user })
         }
     });
     
